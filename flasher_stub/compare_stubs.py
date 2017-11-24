098a28e6a7444da3b0d4b8f9c53d87a46db98c7d
Angus Gratton <gus@projectgus.com>
2017-11-24 15:45:03 +1100

    compare_stubs: Add some helpful diff code

--- flasher_stub/compare_stubs.py
+++ flasher_stub/compare_stubs.py
@@ -6,6 +6,21 @@ import sys
 #
 # (Used by Travis to verify the stubs are up to date.)
 
+def verbose_diff(new, old):
+    for k in [ "data_start", "text_start"]:
+        if new[k] != old[k]:
+            print("New %s 0x%x old %s 0%x" % (k, new[k], old[k]))
+
+    for k in [ "data", "text" ]:
+        if len(new[k]) != len(old[k]):
+            print("New %s %d bytes, old stub code %d bytes" % (k, len(new[k]), len(old)[k]))
+        if new[k] != old[k]:
+            print("%s is different" % k)
+            if len(new[k]) == len(old[k]):
+                for b in range(len(new[k])):
+                    if new[k][b] != old[k][b]:
+                        print("  Byte 0x%x: new 0x%02x old 0x%02x" % (b, ord(new[k][b]), ord(old[k][b])))
+
 if __name__ == "__main__":
     same = True
     sys.path.append("..")
@@ -19,9 +34,11 @@ if __name__ == "__main__":
 
     if esptool.ESP8266ROM.STUB_CODE != old_8266_stub:
         print("ESP8266 stub code in esptool.py is different to just-built stub")
+        verbose_diff(esptool.ESP8266ROM.STUB_CODE, old_8266_stub)
         same = False
     if esptool.ESP32ROM.STUB_CODE != old_32_stub:
         print("ESP32 stub code in esptool.py is different to just-built stub.")
+        verbose_diff(esptool.ESP32ROM.STUB_CODE, old_32_stub)
         same = False
     if same:
         print("Stub code is the same")
