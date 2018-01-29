espressif/esptool

commit bf2ae5131f4ea915ac5f71ac077fa269b4a763ee

Fix using esptool with non serial.Serial instances (i.e. RFC2217, etc.)
Signed-off-by: Alexandr Zarubkin <me21@yandex.ru>

esptool.py
@@ -183,10 +183,10 @@ class ESPLoader(object):
         with ones which throw NotImplementedInROMError().
 
         """
-        if isinstance(port, serial.Serial):
-            self._port = port
-        else:
+        if isinstance(port, str):
             self._port = serial.serial_for_url(port)
+        else:
+            self._port = port
         self._slip_reader = slip_reader(self._port, self.trace)
         # setting baud rate in a separate step is a workaround for
         # CH341 driver on some Linux versions (this opens at 9600 then

commit 8a8b32ae3c53826a38d9f99e3c289cd3369cbc30

Allow ESPLoader() constructor to be called with unicode string on Python 2
Fixes minor regression in bf2ae513.

esptool.py
@@ -99,6 +99,12 @@ else:
     def byte(bitstr, index):
         return bitstr[index]
 
+# Provide a 'basestring' class on Python 3
+try:
+    basestring
+except NameError:
+    basestring = str
+
 
 def esp8266_function_only(func):
     """ Attribute for a function only supported on ESP8266 """
@@ -183,7 +189,7 @@ class ESPLoader(object):
         with ones which throw NotImplementedInROMError().
 
         """
-        if isinstance(port, str):
+        if isinstance(port, basestring):
             self._port = serial.serial_for_url(port)
         else:
             self._port = port