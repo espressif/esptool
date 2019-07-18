1319c49adb2fe99d2981151ff781930d6ed62729
Angus Gratton <angus@espressif.com>
2019-07-18 14:44:14 +1000

    Version 2.7

--- esptool.py
+++ esptool.py
@@ -60,7 +60,7 @@ except ImportError:
           "Check the README for installation instructions." % (sys.VERSION, sys.executable))
     raise
 
-__version__ = "2.7-dev"
+__version__ = "2.7"
 
 MAX_UINT32 = 0xffffffff
 MAX_UINT24 = 0xffffff
