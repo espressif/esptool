df13de2788fd7e701220ef91ba2444f0d2b3f7e3
Yegor Yefremov <yegorslists@googlemail.com>
2019-11-08 10:50:51 +0100

    Replace license header and copyright information with SPDX identifiers
    This approach follows the one suggested by REUSE project.
    
    https://reuse.software/spec/
    
    Closes https://github.com/espressif/esptool/pull/476

--- esptool.py
+++ esptool.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 #
+# SPDX-FileCopyrightText: 2014-2016 Fredrik Ahlberg, Angus Gratton, Espressif Systems (Shanghai) PTE LTD, other contributors as noted.
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# ESP8266 & ESP32 family ROM Bootloader Utility
 # ESP8266 & ESP32 family & ESP8684 ROM Bootloader Utility
-# Copyright (C) 2014-2021 Fredrik Ahlberg, Angus Gratton, Espressif Systems (Shanghai) CO LTD, other contributors as noted.
 # https://github.com/espressif/esptool
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
-# Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from __future__ import division, print_function
 
--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Topic :: Software Development :: Embedded Systems',
         'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPL-2.0-or-later)',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',  # Note: when dropping 3.4 support we can also remove the check in setup_requires
         'Programming Language :: Python :: 3.5',
