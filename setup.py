c6f40c7ec7c27af64ea1025a95fcc3090bcc3257
Angus Gratton <angus@espressif.com>
2020-11-18 19:38:19 +1100

    setup.py: Add Python 3.7 & 3.8 versions in metadata

--- setup.py
+++ setup.py
@@ -100,6 +100,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     extras_require={
         "dev": [
