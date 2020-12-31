8640ae3e82653220dacf7c2188db216c75ed8706
radim.karnis <radim.karnis@espressif.com>
2020-12-31 11:08:18 +0100

    ci: Allow sub-process coverage measurement

---
+++ test/sitecustomize.py
@@ -0,0 +1,5 @@
+import coverage
+coverage.process_startup()
+
+# This file exists to perform arbitrary site-specific customizations.
+# This script is executed before every Python process to start coverage measurement.
