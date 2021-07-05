b5456c1c2a11f851e89227346d7468fc4c9ce729
XiNGRZ <hi@xingrz.me>
2021-07-05 12:58:02 +0800

    esptool: esp32c3: Fix detection of package version
    The chip is always detected as "unknown ESP32-C3" since the bit length of
    the efuse field it reads was wrong.

--- esptool.py
+++ esptool.py
@@ -2056,7 +2056,7 @@ class ESP32C3ROM(ESP32ROM):
         num_word = 3
         block1_addr = self.EFUSE_BASE + 0x044
         word3 = self.read_reg(block1_addr + (4 * num_word))
-        pkg_version = (word3 >> 21) & 0x0F
+        pkg_version = (word3 >> 21) & 0x07
         return pkg_version
 
     def get_chip_revision(self):
