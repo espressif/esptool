01bd2269e11213834a26dbd3b53aa06fc7ff745d
radim.karnis <radim.karnis@espressif.com>
2023-02-08 11:38:45 +0100

    ci(esp32h2): Add target tests

--- test/test_esptool.py
+++ test/test_esptool.py
@@ -181,7 +181,8 @@ class EsptoolTestCase:
         if (
             preload
             and arg_preload_port
-            and arg_chip in ["esp32c3", "esp32s3", "esp32c6"]  # With USB-JTAG/Serial
+            and arg_chip
+            in ["esp32c3", "esp32s3", "esp32c6", "esp32h2"]  # With USB-JTAG/Serial
         ):
             port_index = base_cmd.index("--port") + 1
             base_cmd[port_index] = arg_preload_port  # Set the port to the preload one
