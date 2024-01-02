6ca9b81c2b617923a00fbe935e234f67f5a64252
radim.karnis <radim.karnis@espressif.com>
2024-01-02 13:34:37 +0100

    ci(dev_releasing): Let patch_dev_release.py accept any version specifier

--- ci/patch_dev_release.py
+++ ci/patch_dev_release.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -6,23 +6,20 @@ import argparse
 import re
 
 LINE_RE = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]")
-NEW_LINE = '__version__ = "{}"'
+NEW_LINE = '__version__ = "{}"\n'
 
 
-def get_new_version(old_version, dev_number):
-    assert old_version.endswith("-dev")
-    return old_version.replace("-dev", ".dev{}".format(dev_number), 1)
+def patch_file(path, new_version):
+    assert ".dev" in new_version
+    new_version = new_version.lstrip("v")
 
-
-def patch_file(path, dev_number):
     with open(path, "r") as fin:
         lines = fin.readlines()
 
     for i, line in enumerate(lines, start=0):
         m = LINE_RE.search(line)
         if m:
-            old_version = m.group(1)
-            lines[i] = NEW_LINE.format(get_new_version(old_version, dev_number))
+            lines[i] = NEW_LINE.format(new_version)
             break
 
     with open(path, "w") as fout:
@@ -33,10 +30,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="Path to script with __version__")
     parser.add_argument(
-        "--dev-no", type=int, help="Number N to patch the version to '.devN'"
+        "--version", help="Development version specifier to patch the version to"
     )
     args = parser.parse_args()
-    patch_file(args.file, args.dev_no)
+    patch_file(args.file, args.version)
 
 
 if __name__ == "__main__":
