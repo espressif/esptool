c96f765354dc96b6c9e13442d90debae2b79976c
UniversalBit <lazzaragiuliano@gmail.com>
2025-01-28 01:00:18 +0100

    Update __init__.py

--- esp_rfc2217_server/__init__.py
+++ esp_rfc2217_server/__init__.py
@@ -87,7 +87,7 @@ def main():
 
     srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    srv.bind(("", args.localport))
+    srv.bind(("127.0.0.1", args.localport))
     srv.listen(1)
     logging.info(f"TCP/IP port: {args.localport}")
 
