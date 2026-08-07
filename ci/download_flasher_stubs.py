e881f4a152d056933684db0593764e9e86c530bc
Jaroslav Burian <jaroslav.burian@espressif.com>
2026-08-07 11:13:01 +0200

    feat(stub_flasher): Update flasher stub to v1.2.1
    Enable ESP32-S31 flasher-stub support and 4-byte flash addressing.
    Include the ESP32 CPU clock initialization fix and document ESP32-S31
    support for accessing flash beyond 16 MB.

--- ci/download_flasher_stubs.py
+++ ci/download_flasher_stubs.py
@@ -31,7 +31,7 @@ STUBS = (
     {
         "STUB_SET_VERSION": "2",
         "URL": "https://github.com/espressif/esp-flasher-stub/",
-        "VERSION": "v1.1.0",
+        "VERSION": "v1.2.1",
         "FILE_LIST": (
             "esp32",
             "esp32c2",
